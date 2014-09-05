ff911db0fc76aea949c44bddaf50a74b92a7708f
coldnebo <larry.kyrala@gmail.com>
2014-09-05 08:43:38 -0400

    LocalDispatcher couldn't detect webrick because it hadn't loaded by the time this Engine runs, so I moved it into an :after block.
    This allows webrick to be detected.
    
    Also, the base_urls are being cached on SwaggerYard init, so they were ignoring
    SwaggerYard.config block in the Rails app initializer.  Setting config.reload is
    one way to solve this, but another way is to simply clear the cache after engine
    initialization.
    
    since engine is marked :after, config has already occured in the Rails.app, so
    clearing the cache forces a reload of the correct baseurl.

--- lib/swagger_yard/engine.rb
+++ lib/swagger_yard/engine.rb
@@ -2,17 +2,21 @@ require "swagger_yard/local_dispatcher"
 
 module SwaggerYard
   class Engine < ::Rails::Engine
-    if SwaggerYard::LocalDispatcher.new.server?
-      isolate_namespace SwaggerYard
+    isolate_namespace SwaggerYard
 
-      # NOTE: We should opt for asset pipeline instead of this.
-      initializer 'swagger_yard.load_static_assets' do |app|
-        app.middleware.use(::ActionDispatch::Static, "#{root}/public")
-      end
+    initializer :after do |app|     
+      if SwaggerYard::LocalDispatcher.new.server?
+        # NOTE: We should opt for asset pipeline instead of this.
+        #initializer 'swagger_yard.load_static_assets' do |app|
+          app.middleware.use(::ActionDispatch::Static, "#{root}/public")
+        #end
 
-      initializer "swagger_yard.finisher_hook" do |app|
-        SwaggerYard.generate!("#{app.root}/app/controllers/**/*.rb")
+        #initializer "swagger_yard.finisher_hook" do |app|
+          SwaggerYard.generate!("#{app.root}/app/controllers/**/*.rb")
+        #end
+          SwaggerYard.clear_cache
       end
     end
+
   end
 end
