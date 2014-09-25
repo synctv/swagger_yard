5d86c2970d4857ed15d93c4dad85a012c2cdccb3
Tony Pitale <tony.pitale@livingsocial.com>
2014-09-25 11:20:43 -0400

    remove configuration remnants for caching we no longer support

--- lib/swagger_yard/configuration.rb
+++ lib/swagger_yard/configuration.rb
@@ -2,16 +2,11 @@ module SwaggerYard
   class Configuration
     attr_accessor :swagger_spec_base_path, :api_base_path, :api_path
     attr_accessor :swagger_version, :api_version
-    attr_accessor :cache_store, :cache_prefix
     attr_accessor :enable, :reload
 
     def initialize
       self.swagger_version = "1.1"
       self.api_version = "0.1"
-      # do we automatically need to require Rails?
-      # do we even need caching on this?
-      self.cache_store = Rails.cache
-      self.cache_prefix = "swagger_yard/"
       self.enable = false
       self.reload = true
     end
