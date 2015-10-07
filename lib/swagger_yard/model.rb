af1bd69e74229eda51d1a017f9dc15017bdee1df
Peter Dore <peter.dore@livingsocial.com>
2015-10-07 14:35:11 -0700

    Allow deeply nested model objects
    By recursively listing property models

--- lib/swagger_yard/model.rb
+++ lib/swagger_yard/model.rb
@@ -45,6 +45,17 @@ module SwaggerYard
       @properties.map(&:model_name).compact
     end
 
+    def recursive_properties_model_names(model_list)
+      properties_model_names + properties_model_names.map do |model_name|
+        child_model = model_from_model_list(model_list, model_name)
+        child_model.recursive_properties_model_names(model_list) if child_model
+      end.compact
+    end
+
+    def model_from_model_list(model_list, model_name)
+      model_list.find{|model| model.id == model_name}
+    end
+
     def to_h
       raise "Model is missing @model tag" if id.nil?
 
